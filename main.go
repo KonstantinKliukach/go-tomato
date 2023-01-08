cf2f83d3e0ea06e558c5a0b7613262281269a566
Константин Клюкач <jfrost@192.168.1.4>
2023-01-08 22:42:11 +0200

    [feature] control from commands

--- main.go
+++ main.go
@@ -20,29 +20,27 @@ func getNextTomatoName(num int) string {
 }
 
 func main() {
-	current := 1
-	t := tomato.GetAllTomatoes()
+	// current := 1
+	allT := tomato.GetAllTomatoes()
 
 	for {
-		fmt.Printf("Next tomato is: %s\n", getNextTomatoName(current))
-		fmt.Printf("If you are ready, print \"yes\" or one of these commands:\n%s or press \"no\" to exit programm", tomato.GetListOfCommands(t))
+		// fmt.Printf("Next tomato is: %s\n", getNextTomatoName(current))
+		fmt.Printf("If you are ready, print \"yes\" or one of these commands:\n%s or press \"no\" to exit programm", tomato.GetListOfCommands(allT))
 		i := make(chan string, 1)
 
 		go utils.GetInput(i)
 
 		s := <-i
 
-		if s == "yes" {
-			if current%2 != 0 {
-				t["work"].Start()
-			} else if current%4 == 0 {
-				t["sr"].Start()
-			} else {
-				t["lr"].Start()
-			}
-		} else if s == "no" {
+		if s == "no" {
 			fmt.Println("See you, space cowboy")
 			os.Exit(0)
+		}
+
+		value, ok := allT[s]
+
+		if ok {
+			value.Start()
 		}
 	}
 }
